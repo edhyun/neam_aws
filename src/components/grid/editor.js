171ca8666a015d6f04dc797fa3a7795f79549393
Dahyun Eun <powwel@naver.com>
2017-06-26 16:29:00 +0000

    init2

---
+++ src/components/grid/editor.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Editor extends Component {
+  render() {
+    return(
+      <div>
+        <div className="editor-toolbar">
+          editor-toolbar
+        </div>
+        <div className="editor-content">
+          editor content
+        </div>
+      </div>
+    )
+  }
+}
